eb39705961083d6dd60e39cb7820f267ebf45067
SannyJack <sannyjack28@gmail.com>
2022-08-01 20:17:15 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Heloo World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, vel!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
